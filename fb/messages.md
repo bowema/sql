0446c434285d04719553a79dd2dd103ea3b9e945
Bowen Ma <bma10@dons.usfca.edu>
2019-06-18 22:48:15 -0700

    messages

--- fb/messages.md
+++ fb/messages.md
@@ -1,9 +1,8 @@
-table:  
+# Messages 
+Table:  
 date | user_A | user_B | number_messages
 
 1)	what can you tell from this table  
 2)	Write a query for the distribution of number of messages for each user. How the distribution is gonna look like and why? 
-
 3)	Write a query find the top partner for each user (most messages)  
-
 4)	SUM(n_msg_with_top_partner)/SUM(all_messages_with_all_contacts），sum over all users  
--- fb/messages.sql
+++ fb/messages.sql
@@ -1,4 +1,19 @@
+-- 2)  Write a query for the distribution of number of messages for each user. How the distribution is gonna look like and why? 
+-- output 2 columns: X: number of unique messages for each user; Y: number of user with this many messages
+SELECT num_msg, COUNT(user_A) AS user_cnt 
+FROM 
+    (SELECT user_A, SUM(number_messages) AS num_msg
+     FROM fb.messages
+     GROUP BY user_A, user_B
+     UNION ALL
+     SELECT user_B, SUM(number_messages) AS num_msg
+     FROM fb.messages
+     GROUP BY user_B, user_A) AS InnerQ
+GROUP BY num_msg;
+
+
 -- (3) Write a query find the top partner for each user (most messages)
+-- use union all
 
 SELECT user_A, user_B, num_msg
 FROM (
