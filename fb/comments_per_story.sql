0a37f39d0867945103dc3f6149c82baf61383fee
Bowen Ma <40877740+bowema@users.noreply.github.com>
2019-07-01 17:52:17 -0700

    Update comments_per_story.sql

--- fb/comments_per_story.sql
+++ fb/comments_per_story.sql
@@ -1,6 +1,8 @@
 -- Content Actions - calculate number of comments per story
--- Don't overcomplicate this problem. Use a left join to add comment info to each story, then filter out comments, 
--- lastly group by unique story (assume content_id is unique per story) and count number of target_id's which is number of comments.
+-- Don't overcomplicate this problem. 
+-- 1 Use a left join to add comment info to each story (post/photo), since target_id indicates which story has comment
+-- 2 then filter out comments from content_type (such that we have only stories) 
+-- 3 lastly group by unique story (assume content_id is unique per story) and count number of target_id's which is number of comments.
 
 SELECT t1.content_id, COUNT(t2.target_id)
 FROM fb.content_actions t1 
