f6365669ebdeaba0f6bfcd3a65381d44ac65f7b3
Bowen Ma <bma10@dons.usfca.edu>
2019-08-15 11:04:03 -0700

    570

--- README.md
+++ README.md
@@ -14,6 +14,7 @@
 |550| [Game Play Analysis IV](https://zqt0.gitbook.io/leetcode/sql/550.-game-play-analysis-iv-zhong-deng) |[PostgreSQL](leetcode/550.sql) |Medium|
 |570| [Managers with at Least 5 Direct Reports](https://zqt0.gitbook.io/leetcode/sql/570.medium-zhi-shao-you-5-ming-zhi-jie-xia-shu-de-jing-li)| [MySQL](leetcode/570.sql) |Medium|
 
+
 # Facebook
 | # | Problem | Solution| Concept/Comment|
 |---|---|---|----|
--- leetcode/570.sql
+++ leetcode/570.sql
@@ -12,9 +12,20 @@
 -- INSERT INTO lc.employee VALUES (105, 'Anne', 'A', 101);
 -- INSERT INTO lc.employee VALUES (106, 'Ron', 'B', 101);
 
-
+-- alternative 1
 SELECT a.name
 FROM lc.employee a
 LEFT JOIN lc.employee b ON a.id = b.managerId
 GROUP BY a.name
 HAVING COUNT(b.id) >= 1;
+
+
+-- alternative 2
+SELECT NAME  -- join name from employee table to the manager id to get the name of manager with >= direct reports
+FROM lc.employee AS t1
+    JOIN
+    (SELECT managerId from lc.employee
+    GROUP BY 1
+    HAVING COUNT(managerId)>=5) AS t2  -- t2 has managerid with >= 5 direct reports
+    ON t1.Id = t2.managerId;
+-- https://leetcode.com/articles/managers-with-at-least-5-direct-reports/
